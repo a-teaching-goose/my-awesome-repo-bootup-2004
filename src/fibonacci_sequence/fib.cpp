cf5bbfd9c5b05d7a50004006ecd1d6f28bbfaba5
Peng Du <rick.peng.du@gmail.com>
2024-01-06 10:57:17 -0800

    pass unit test

--- src/fibonacci_sequence/fib.cpp
+++ src/fibonacci_sequence/fib.cpp
@@ -3,6 +3,19 @@
 /*
  * [0, 1, 1, 2, 3, 5, ....]
  */
-uint fib(const uint n) {
-    return -1; // TODO: replace this placeholder with your real code
+uint fib(const uint n) {    // nth fib number
+    uint *data = new uint[n + 1];
+    if (n < 2) {
+        return n;
+    }
+
+    data[0] = 0;
+    data[1] = 1;
+
+    for (int i = 2; i <= n; ++i) {
+        data[i] = data[i - 1] + data[i - 2];
+    }
+
+    return data[n];
+
 }
